c921af9b9ff329ead1cff05836e3fb1a1cc396eb
ashwini3196 <ashwiniy.02@gmail.com>
2017-07-16 02:21:23 +0530

    update_form

--- analyse.php
+++ analyse.php
@@ -3,51 +3,6 @@
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-}
-
-.container {
-  padding: 0 16px;
-}
-
-.container::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-
-.title {
-  color: grey;
-  font-size: 18px;
-}
-
-button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-  font-size: 18px;
-}
-
-a {
-  text-decoration: none;
-  font-size: 22px;
-  color: black;
-}
-
-button:hover, a:hover {
-  opacity: 0.7;
-}
 </style>
 </head>
 <body>
@@ -80,6 +35,9 @@ echo '<h2 style="text-align:center">User Profile</h2>
       <p>PARENTAL-STATUS:'.$row["parental_status"].'</p>
 	  <p>FAMILY-INCOME:'.$row["family_income"].'</p>
 	  </div></div>'
+$sql = "select term,avg from student_assess where sid=39";
+$result = $conn->query($sql);	
+$row = $result->fetch_assoc();  
 ?>
 </body>
 </html>
