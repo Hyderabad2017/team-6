a1f153410094f1336c39a0148b321781ab419c43
ashwini3196 <ashwiniy.02@gmail.com>
2017-07-16 03:50:01 +0530

    loginupdated

--- log.php
+++ log.php
@@ -1,31 +1,25 @@
-<?
+<?php
 session_start();
-
 $server = "localhost";
 $username = "root";
 $password = "";
 $db_name = "learning_curve";
-
 $db = mysql_connect($server,$username,$password) or die("Connection to database failed, perhaps the service is down !!");
 mysql_select_db($db_name) or die("Database name not available !!");
-
+$user=$_POST['user'];
+$pass=$_POST['pass'];
  $query=mysql_query("SELECT * FROM login_details WHERE username='".$user."' AND password='".$pass."'");  
     $numrows=mysql_num_rows($query);  
-    if($numrows!=0)  
+    if($numrows==1)  
     {  
-    while($row=mysql_fetch_assoc($query))  
-    {  
-    $dbusername=$row['username'];  
-    $dbpassword=$row['password'];  
-    }  
- if($user == $dbusername && $pass == $dbpassword)  
-    {  
-echo "connecting";
+      
+    echo "connecting";
 //header("Location: securedpage.php");
-}
+  }
 else
 {
 echo "not connecting";
 //header("Location: log.php");
 }
+	
 ?>
