e8abe54a74498640301e622672d27508b4ab3570
Alekhya-Kondapuram <alekhya.kondapuram@gmail.com>
2017-07-16 04:43:30 +0530

    login

--- log.php
+++ log.php
@@ -1,25 +1,26 @@
 <?php
 session_start();
+
 $server = "localhost";
 $username = "root";
 $password = "";
-$db_name = "learning_curve";
+$db_name = "student";
+
 $db = mysql_connect($server,$username,$password) or die("Connection to database failed, perhaps the service is down !!");
 mysql_select_db($db_name) or die("Database name not available !!");
-$user=$_POST['user'];
-$pass=$_POST['pass'];
- $query=mysql_query("SELECT * FROM login_details WHERE username='".$user."' AND password='".$pass."'");  
-    $numrows=mysql_num_rows($query);  
-    if($numrows==1)  
-    {  
-      
-    echo "connecting";
-//header("Location: securedpage.php");
-  }
+
+$login = mysql_query("select * from login_details where (username = '" . $_REQUEST['username'] . "') and (password = '" . ($_REQUEST['password']) . "')",$db);
+$rowcount = mysql_num_rows($login);
+if ($rowcount == 1) {
+$_SESSION['username'] = $_REQUEST['username'];
+echo "connecting";
+
+}
 else
 {
 echo "not connecting";
-//header("Location: log.php");
+header("Location: form.html");
+exit;
 }
-	
+
 ?>
