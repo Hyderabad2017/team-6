71edab90c0d60f2514659bd6a77c9290f12f250d
100gbps <kajitsai7@gmail.com>
2017-07-15 23:36:09 +0530

    add to std analysis

---
+++ std_add.php
@@ -0,0 +1,36 @@
+ <?php
+$servername = "localhost"; // add you user name pass and db name
+$username = "root";
+$password = "";
+$dbname = "learning_curve";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$term = $_REQUEST["term"];
+$sid = $_REQUEST["sid"];
+$school = $_REQUEST["school"];
+$q1 = $_REQUEST["q1"];
+$q2 = $_REQUEST["q2"];
+$q3 = $_REQUEST["q3"];
+$q4 = $_REQUEST["q4"];
+$q5 = $_REQUEST["q5"];
+$c1 = $_REQUEST["c1"];
+$c2 = $_REQUEST["c2"];  
+$c3 = $_REQUEST["c3"];
+$c4 = $_REQUEST["c4"];
+$c5 = $_REQUEST["c5"];
+$avg = ($q1 + $q2 + $q3 + $q4 + $q5)/5;
+$sql = "INSERT INTO student_assess (term,sid,school,q1,q2,q3,q4,q5,c1,c2,c3,c4,c5)  
+VALUES ('$term','$sid','$school','$q1','$q2','$q3','$q4','$q5','$c1','$c2','$c3','$c4','$c5')"; //adding data to database
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
