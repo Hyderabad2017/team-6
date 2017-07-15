3e2c4fe6737f8f029ba691f4a414bdf33d0f751f
bhavishyaa <bhavishyareddy97@email.com>
2017-07-16 00:40:29 +0530

    changes

---
+++ upload_cr_assess.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+<form action="u_cr_term1.php" method="post" enctype="multipart/form-data">
+    Select file for classroom assessment of term 1:<br>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload file" name="submit">
+</form>
+<br><br><br>
+<form action="u_cr_term2.php" method="post" enctype="multipart/form-data">
+    Select file for classroom assessment of term 2:<br>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload file" name="submit">
+</form>
+<br><br><br>
+<form action="u_cr_term3.php" method="post" enctype="multipart/form-data">
+    Select file for classroom assessment of term 3:<br>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload file" name="submit">
+</form>
+
+</body>
+</html>
