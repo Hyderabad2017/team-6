70f8645c3514da278c81a1c5e83f7a6b834ee93b
shenaznarsidani <narsidanishenaz@gmail.com>
2017-07-16 01:57:30 +0530

    select.php- to select stu or cr assess

---
+++ select.php
@@ -0,0 +1,11 @@
+<html>
+<form action ="upload.php">
+Upload Student Assessment Data:  
+<input type="Submit" value="Student assessment Data">
+</form>
+
+<form action="upload_cr_assess.php">
+Upload Classroom Assessment Data:  
+<input type="Submit" value="Classroom Assessment Data">
+</form>
+</html>
